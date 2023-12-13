ac70f362f3a35146dd8f30d8873cf6daec724fe8
julienhatemian <julien-hat.62@hotmail.fr>
2023-12-13 20:44:42 +0100

    Database initialisation

---
+++ dB/dbConfig.php
@@ -0,0 +1,9 @@
+<?php
+namespace Blindtest\database;
+
+abstract class dbConfig{
+    protected string $host = 'loclahost';
+    protected string $dataBase = 'blindtest';
+    protected string $user = 'root';
+    protected string $password = '';
+}
