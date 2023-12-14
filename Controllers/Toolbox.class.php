211e49d5af8bdaf15e6d1181bfc6e52167c91d09
julienhatemian <julien-hat.62@hotmail.fr>
2023-12-14 05:16:51 +0100

    Initialisation toolbox.class

---
+++ Controllers/Toolbox.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Blindtest\Controllers;
+class Toolbox
+{
+    public const RED_COLOR = "alert-danger";
+    public const ORANGE_COLOR = "alert-warning";
+    public const GREEN_COLOR = "alert-success";
+    
+    public static function addAlertMessage($message, $type){
+        $_SESSION['alert'][] = [
+            "message" => $message,
+            "type" => $type
+        ];
+    }
+}
