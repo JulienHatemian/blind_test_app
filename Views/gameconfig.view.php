c7d1d22f6e97e1109187c76762c593c29a87c0f6
julienhatemian <julien-hat.62@hotmail.fr>
2023-12-18 07:03:06 +0100

    Edit form configuration

--- Views/gameconfig.view.php
+++ Views/gameconfig.view.php
@@ -1,6 +1,6 @@
 <h1>Blintest - Configuration</h1>
 <form action="#" method="post" id="form-config">
-    <div class="form-input">
+    <div class="form-group">
         <legend>Genres:</legend>
         <?php if(isset($genres)) :?>
         <?php foreach($genres as $genre): ?>
@@ -9,7 +9,7 @@
         <?php endforeach; endif; ?>
     </div>
     
-    <div class="form-input">
+    <div class="form-group">
         <legend>Types:</legend>
         <?php if(isset($types)) :?>
         <?php foreach($types as $type): ?>
@@ -18,8 +18,21 @@
         <?php endforeach; endif; ?>
     </div>
 
-    <div class="form-input">
-        <legend>Mode de jeu:</legend>
+    <div class="form-group">
+        <div class="form-item">
+            <label for="timer">Timer:</label>
+            <input type="number" value="10" id="timer" name="timer"> second(s)
+            <small>Min.: 1 sec - Max.: 30 sec</small>
+        </div>
+        <div class="form-item">
+            <label for="rounds">Rounds:</label>
+            <input type="number" value="10" id="rounds" name="rounds">
+            <small>Min.: 1 round - Max.: 30 rounds</small>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <legend>Game mode:</legend>
         <?php if(isset($gamemodes)) :?>
         <?php foreach($gamemodes as $gamemode): ?>
             <button type="submit" class="button-type" href="#"><?= $gamemode['libelle'] ?></button>
--- Views/homepage.view.php
+++ Views/homepage.view.php
@@ -1,5 +1,5 @@
 <h2>Get ready to start a new Blindtest !</h2>
 
 <div class="cadre">
-    <a type="button" class="button-type" href="<?= URL; ?>gameconfig">Get Started</a>
+    <a type="button" class="button-type" href="<?= URL; ?>gameconfig">Get Started !</a>
 </div>
