56af1b5f4a6f1d6f08f750d78d6fcc44d7ac8221
julienhatemian <julien-hat.62@hotmail.fr>
2024-01-04 08:44:12 +0100

    CheckConfig correction

--- Controllers/Check.php
+++ Controllers/Check.php
@@ -25,17 +25,17 @@ class Check
     }
     public function checkConfig(array $genres, array $types, int $gamemode, int $rounds, int $timer)
     {
-        var_dump($types);
-        var_dump(array_search(false, array_map('is_numeric', $types)));
-        var_dump($genres);
-        exit;
+        // var_dump($types);
+        // var_dump(array_search(false, array_map('is_numeric', $types)));
+        // var_dump($genres);
+        // exit;
         if(empty($genres) || empty($types)){
             Toolbox::addAlertMessage(
                 "Please, choose at least 1 genre & 1 type.",
                 Toolbox::RED_COLOR
             );
         }else{
-            if(array_search(false, array_map('is_numeric', $genres)) != false || array_search(false, array_map('is_numeric', $types)) != false){
+            if(array_search(false, array_map('is_numeric', $genres)) !== false || array_search(false, array_map('is_numeric', $types)) !== false){
                 Toolbox::addAlertMessage(
                     "Please, choose a valid genre and type.",
                     Toolbox::RED_COLOR
