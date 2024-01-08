ddd131e570ab90824bbab85dce7d7bc3c5f1e11c
julienhatemian <julien-hat.62@hotmail.fr>
2024-01-08 20:31:44 +0100

    Ajax response data

--- blindtestApi.php
+++ blindtestApi.php
@@ -5,8 +5,6 @@
 
     $blindtestservice = new BlindtestService;
 
-    var_dump($data);
-    exit;
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $data = json_decode(file_get_contents('php://input'), true);
         var_dump($data);
