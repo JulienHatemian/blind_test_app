c49ee3f5a50afa928efe6115d851e1703ff378d9
julienhatemian <julien-hat.62@hotmail.fr>
2024-04-06 05:48:55 +0200

    Edit and adapt gameconfig form view and css

--- Views/gameconfig.view.php
+++ Views/gameconfig.view.php
@@ -2,43 +2,56 @@
     <h2>Blintest - Configuration</h2>
     <form action="<?= URL; ?>blindtest" method="post" id="form-config">
         <div class="form-group">
-            <legend>Genres:</legend>
             <?php if(isset($genres)) :?>
-            <?php foreach($genres as $genre): ?>
-                <input type="checkbox" name="genre[]" value="<?= $genre['idgenre'] ?>" id="checkGenre<?= $genre['libelle'] ?>">
-                <label for="checkGenre<?= $genre['libelle'] ?>"><?= $genre['libelle'] ?></label>
-            <?php endforeach; endif; ?>
+                <legend>Genres:</legend>
+                <div class="checkbox-container d-flex flex-row">
+                    <?php foreach($genres as $genre): ?>
+                        <div class="checkbox">
+                            <input type="checkbox" name="genre[]" value="<?= $genre['idgenre'] ?>" id="checkGenre<?= $genre['libelle'] ?>">
+                            <label for="checkGenre<?= $genre['libelle'] ?>"><?= $genre['libelle'] ?></label>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
         </div>
         
         <div class="form-group">
-            <legend>Types:</legend>
             <?php if(isset($types)) :?>
-            <?php foreach($types as $type): ?>
-                <input type="checkbox" name="type[]" value="<?= $type['idtype'] ?>" id="checkType<?= $type['libelle'] ?>">
-                <label for="checkType<?= $type['libelle'] ?>"><?= $type['libelle'] ?></label>
-            <?php endforeach; endif; ?>
+                <legend>Types:</legend>
+                <div class="checkbox-container d-flex flex-row">
+                    <?php foreach($types as $type): ?>
+                        <div class="checkbox">
+                            <input type="checkbox" name="type[]" value="<?= $type['idtype'] ?>" id="checkType<?= $type['libelle'] ?>">
+                            <label for="checkType<?= $type['libelle'] ?>"><?= $type['libelle'] ?></label>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
         </div>
 
         <div class="form-group">
             <!-- <div class="form-item"> -->
-                <label for="timer">Timer:</label>
+                <legend>Timer:</legend>
                 <input type="number" value="10" id="timer" name="timer" min="1" max="30"> second(s)
+                <br>
                 <small>Min.: 1 sec - Max.: 30 sec</small>
         </div>
         <div class="form-group">
             <!-- <div class="form-item"> -->
-                <label for="rounds">Rounds:</label>
+                <legend>Rounds:</legend>
                 <input type="number" value="10" id="rounds" name="rounds" min="1" max="30">
+                <br>
                 <small>Min.: 1 round - Max.: 30 rounds</small>
             <!-- </div> -->
         </div>
 
         <div class="form-group">
-            <legend>Game mode:</legend>
             <?php if(isset($gamemodes)) :?>
-            <?php foreach($gamemodes as $gamemode): ?>
-                <button type="submit" class="button-type" name="gamemode" value="<?= $gamemode['idgamemode'] ?>" <?php echo(($gamemode['active'] == 0) ? 'disabled' : '') ?>><?= $gamemode['libelle'] ?></button>
-            <?php endforeach; endif; ?>
+                <legend>Game mode:</legend>
+                <?php foreach($gamemodes as $gamemode): ?>
+                    <button type="submit" class="button-type" name="gamemode" value="<?= $gamemode['idgamemode'] ?>" <?php echo(($gamemode['active'] == 0) ? 'disabled' : '') ?>><?= $gamemode['libelle'] ?></button>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </form>
 </div>
